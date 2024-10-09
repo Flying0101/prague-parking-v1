cb6ddd5512f2efb52ce5ef173dcaa098899e77f8
Stephano <stephano@live.se>
2024-10-09 17:57:47 +0200

    fix: validation for registrationNumber and type of vehicle.

--- Vehicle.cs
+++ Vehicle.cs
@@ -3,13 +3,21 @@
     public class Vehicle
     {
         public string RegistrationNumber { get; private set; }
-        public string Type { get; private set; } // "type of vehicle"
+        public string Type { get; private set; }
 
         public Vehicle(string registrationNumber, string type)
         {
-            if (registrationNumber.Length > 10)
-                throw new ArgumentException("Registration number can't be more than 10 characters.");
+            if (registrationNumber.Length > 10 || registrationNumber.Length <= 0)
+            {
+                Console.Clear();
+                throw new ArgumentException("Registration number can't be more than 10 characters or less than 1.\n");
 
+            }
+            if (type != "car" && type != "motorcycle")
+            {
+                Console.Clear();
+                throw new ArgumentException("Invalid vehicle type, only car or motorcycle.\n");
+            }
             RegistrationNumber = registrationNumber;
             Type = type;
         }
