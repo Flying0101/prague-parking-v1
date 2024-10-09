18dbe1c968a1e849dc3c81263dfb85222ea55614
Stephano <stephano@live.se>
2024-10-09 14:35:44 +0200

    add: main file

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace prague_parking_v1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to pargue parkings first commit.");
+        }
+    }
+}
