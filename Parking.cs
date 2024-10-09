04e97eb97280e22898444b900a00d3b818d7af03
Stephano <stephano@live.se>
2024-10-09 16:17:12 +0200

    add: new class with empty methods and array with fixed length of 100.

---
+++ Parking.cs
@@ -0,0 +1,34 @@
+﻿namespace prague_parking_v1
+{
+    public class ParkingLot
+    {
+
+        private string[] parkingSpots;
+
+        // constructor
+        public ParkingLot()
+        {
+            parkingSpots = new string[100];
+        }
+
+        public void ParkVehicle(Vehicle vehicle, int spotNumber)
+        {
+            // Implement logic to park the vehicle
+        }
+
+        public void MoveVehicle(int fromSpot, int toSpot)
+        {
+            // change to bool return type // add logic
+        }
+
+        public void RemoveVehicle(int spotNumber)
+        {
+            // change to bool return type // add logic
+        }
+
+        public void FindVehicle(string registrationNumber)
+        {
+            // change to bool return type // add logic
+        }
+    }
+}
