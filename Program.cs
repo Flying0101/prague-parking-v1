f076c31ea32de88e1aff0c315fcfd1dbe76fa420
Stephano <stephano@live.se>
2024-10-09 15:30:05 +0200

    add: user meny functionality and validation for input.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to pargue parkings first commit.");
+            Console.WriteLine("Welcome to Stephano and Ömers parking assignment.");
+
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("---Prague Parking System V1---\n");
+                Console.WriteLine("1. Park a vehicle");
+                Console.WriteLine("2. Move a vehicle");
+                Console.WriteLine("3. Remove a vehicle");
+                Console.WriteLine("4. Search for a vehicle");
+                Console.WriteLine("5. Exit");
+
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        running = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Invalid, please enter a number between 1-5...\n");
+                        break;
+                }
+            }
+
         }
     }
 }
