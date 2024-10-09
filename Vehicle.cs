e3c491105cfa207dd0e873ee4fa916ce10c5a06e
Stephano <stephano@live.se>
2024-10-09 16:19:21 +0200

    add: new vehicle class with constructor with validation for registration number length.

---
+++ Vehicle.cs
@@ -0,0 +1,17 @@
+﻿namespace prague_parking_v1
+{
+    public class Vehicle
+    {
+        public string RegistrationNumber { get; private set; }
+        public string Type { get; private set; } // "type of vehicle"
+
+        public Vehicle(string registrationNumber, string type)
+        {
+            if (registrationNumber.Length > 10)
+                throw new ArgumentException("Registration number can't be more than 10 characters.");
+
+            RegistrationNumber = registrationNumber;
+            Type = type;
+        }
+    }
+}
