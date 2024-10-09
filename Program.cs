3eb57da89b5f00161a962c3e4f085a3f0c507063
Stephano <stephano@live.se>
2024-10-09 18:01:16 +0200

    fix: validation for input, create vehicle and park vehicle.

--- Program.cs
+++ Program.cs
@@ -17,13 +17,41 @@
                 Console.WriteLine("2. Move a vehicle");
                 Console.WriteLine("3. Remove a vehicle");
                 Console.WriteLine("4. Search for a vehicle");
-                Console.WriteLine("5. Exit");
+                Console.WriteLine("5. Exit\n");
 
-                string userInput = Console.ReadLine();
+                string userInput = Console.ReadLine()!;
 
                 switch (userInput)
                 {
                     case "1":
+                        try
+                        {
+                            // Get vehicle registration number from the user
+                            Console.Write("Enter registration number, max 10 characters: ");
+                            string registrationNumber = Console.ReadLine()!;
+
+                            // Get vehicle type from the user
+                            Console.Write("Enter vehicle type, Car / Motorcycle: ");
+                            string vehicleType = Console.ReadLine()!;
+
+                            // Get the parking spot number from the user
+                            Console.Write("Enter parking spot number, 1-100: ");
+                            string inputSpotString = Console.ReadLine()!;
+
+                            // Park the vehicle using the ParkVehicle method
+                            if (parkingLot.ParkVehicle(registrationNumber, vehicleType.ToLower(), inputSpotString))
+                            {
+                                Console.WriteLine($"\nVehicle {registrationNumber} parked successfully in spot {inputSpotString}.\n");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"\nSpot {inputSpotString} is already taken. Please try another spot.\n");
+                            }
+                        }
+                        catch (ArgumentException ex)
+                        {
+                            Console.WriteLine($"Error: {ex.Message}");
+                        }
                         break;
                     case "2":
                         break;
@@ -33,6 +61,9 @@
                         break;
                     case "5":
                         running = false;
+                        Console.WriteLine("\nShutting down program...");
+                        Thread.Sleep(1000);
+
                         break;
                     default:
                         Console.Clear();
